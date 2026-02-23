c91dfaea3718ca9ae19104309eed84c2a06da75f
Mariana <1110366789@u.icesi.edu.co>
2026-02-22 20:24:56 -0500

    Finish

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
-const getPosts = async () => {
-    const response = await response.json() 
-    return result
-}
+const URL = 'http://localhost:3000/posts';
 
-getPosts().then(r => console.log(r))
+const getPosts = () => {
+    fetch(URL)
+        .then(res => res.json())
+        .then(posts => {
+            const container = document.getElementById('posts-container');
+            container.innerHTML = '';
+
+            posts.forEach(post => {
+                container.innerHTML += `
+                    <div class="post-card">
+                        <img src="${post.image}" alt="post">
+                        <h3>${post.title}</h3>
+                        <p>${post.description}</p>
+                        <button class="create btn-delete" onclick="deletePost(${post.id})">Eliminar</button>
+                    </div>
+                `;
+            });
+        });
+};
+
+
+const createPost = () => {
+    const image = document.getElementById('image-url').value;
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+
+
+    if (!image || !title || !description) {
+        return alert("Por favor, llena todos los campos");
+    }
+
+    const newPost = { image, title, description };
+
+    fetch(URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newPost)
+    })
+    .then(() => {
+        getPosts(); 
+        document.getElementById('image-url').value = '';
+        document.getElementById('title').value = '';
+        document.getElementById('description').value = '';
+    });
+};
+
+const deletePost = (id) => {
+    fetch(`${URL}/${id}`, { method: 'DELETE' })
+        .then(() => getPosts());
+};
+
+document.getElementById('submit').onclick = createPost;
+document.getElementById('list').onclick = getPosts;
+
+getPosts();
